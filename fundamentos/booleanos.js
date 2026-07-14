a0bada1f719f140c015738f8375ddcbf3598430d
Murilo <murilodo013@gmail.com>
2026-07-13 22:53:24 -0300

    Exercicios sobre boolean e sobre template Strings

---
+++ fundamentos/booleanos.js
@@ -0,0 +1,33 @@
+let isAtivo = false;
+console.log(isAtivo);
+
+isAtivo = true;
+console.log(isAtivo);
+
+isAtivo = 1;
+console.log(!!isAtivo);// se colocar 2 !! o resultado fica true e se colocar 1 ! fica false porque o ! significa nao
+
+console.log('os verdadeiros.....');
+console.log(!!3);//duas ! porque é uma negação duas vezes oque converte pra verdadeiro ou falso
+console.log(!!-1);
+console.log(!!' ');
+console.log(!!'texto');
+console.log(!![]);
+console.log(!!{});
+console.log(!!Infinity);
+console.log(!!(isAtivo = true));
+
+console.log("os falsos....");
+console.log(!!0);
+console.log(!!"");
+console.log(!!null);
+console.log(!!NaN);
+console.log(!!undefined);
+console.log(!!(isAtivo = false));
+
+console.log('pra finalizar.....');
+console.log(!! ' || null || 0 || "epa" || 123) ' );
+
+let nome = 'Lucas'
+
+console.log(nome || 'Desconhecido')
